d8a2662bd8877da1d4cc0322f180a07de061de30
Alexander Andreev <a.andreev@yadro.com>
2023-12-25 15:52:49 +0300

    update

--- helpers.js
+++ helpers.js
@@ -1,62 +1,62 @@
-const Handlebars = require('handlebars');
+const Handlebars = require("handlebars");
+const nconf = require("nconf");
 
-Handlebars.registerHelper("inc", function(value, options) {
+Handlebars.registerHelper("inc", function (value, options) {
   return parseInt(value) + 1;
 });
 
-Handlebars.registerHelper('reportStory', function(key, fields) {
+Handlebars.registerHelper("reportStory", function (key, fields) {
   const prefix = {
-    '3': 'Working on',
-    '6': 'Implemented',
-    '10104': 'Started a code review for'
+    "In Progress": "Working on",
+    Done: "Implemented",
+    10104: "Started a code review for",
   };
   const suffix = {
-    '2': 'Won\'t fix',
-    '5': 'Not reproducible'
+    2: "Won't fix",
+    5: "Not reproducible",
   };
 
-  let ret = prefix[fields.status.id] || fields.status.name;
-  ret += ` ${key}: "${fields.summary}"`;
+  let ret = prefix[fields.status.name] || fields.status.name;
+  ret += ` ${nconf.get("JIRA_BROWSE_LINK")}${key}: "${fields.summary}"`;
 
   if (fields.resolution && suffix[fields.resolution.id]) {
-    return ret += ` as ${suffix[fields.resolution.id]}`;
+    return (ret += ` as ${suffix[fields.resolution.id]}`);
   }
 
   return ret;
 });
 
-Handlebars.registerHelper('reportBug', function(key, fields) {
+Handlebars.registerHelper("reportBug", function (key, fields) {
   const prefix = {
-    '3': 'Working on',
-    '6': 'Fixed',
-    '10104': 'Started a code review for'
+    "In Progress": "Working on",
+    Done: "Fixed",
+    10104: "Started a code review for",
   };
-  const prio = {
-    '2': 'P1',
-    '3': 'P2'
-  };
+  const prio = {};
   const suffix = {
-    '2': 'Won\'t fix',
-    '5': 'Not reproducible'
+    2: "Won't fix",
+    5: "Not reproducible",
   };
 
-  let ret = prefix[fields.status.id] || fields.status.name;
-  ret += ` ${prio[fields.priority.id] || fields.priority.name}`;
-  ret += ` ${fields.issuetype.name.toLowerCase()} ${key}: "${fields.summary}"`;
+  let ret = prefix[fields.status.name] || fields.status.name;
+  ret += ` ${prio[fields.priority.id] || fields.priority.name} severity`;
+  ret += ` ${fields.issuetype.name.toLowerCase()} ${nconf.get(
+    "JIRA_BROWSE_LINK"
+  )}${key}: "${fields.summary}"`;
 
   if (fields.resolution && suffix[fields.resolution.id]) {
-    return ret += ` as ${suffix[fields.resolution.id]}`;
+    return (ret += ` as ${suffix[fields.resolution.id]}`);
   }
 
   return ret;
 });
 
-Handlebars.registerHelper('reportMergeRequest', function(mergeRequest) {
-  const { title, reference, web_url } = mergeRequest
-  return `${title} ${reference}`
+Handlebars.registerHelper("reportPullRequest", function (pullRequest) {
+  const { title, state } = pullRequest;
+  return `${state} ${title}`;
 });
 
-Handlebars.registerHelper('reportPromotion', function(mergeRequest) {
-  const { title } = mergeRequest
-  return `Promoted ${title} to PhaseII`
+Handlebars.registerHelper("reportPromotion", function (mergeRequest) {
+  const { title } = mergeRequest;
+  return `Promoted ${title} to PhaseII`;
 });
--- index.js
+++ index.js
@@ -1,77 +1,124 @@
 #!/usr/bin/env node
 
-const jiraClient = require('jira-client');
-const { Gitlab } = require('@gitbeaker/node');
-const nconf = require('nconf');
-const Handlebars = require('handlebars');
-const helpers = require('./helpers');
+// const { Gitlab } = require("@gitbeaker/node");
+const base64 = require("base-64");
+const Handlebars = require("handlebars");
+const helpers = require("./helpers");
+const jiraClient = require("jira-client");
+const nconf = require("nconf");
 require("dotenv").config();
 
 // Init config
-nconf
-  .file('config.json')
-  .env()
+nconf.file("config.json").env();
 
-if (nconf.get('JIRA_HOST')) nconf.set('jira:host', nconf.get('JIRA_HOST'));
-if (nconf.get('JIRA_USER')) nconf.set('jira:username', nconf.get('JIRA_USER'));
-if (nconf.get('JIRA_PASSWORD')) nconf.set('jira:password', nconf.get('JIRA_PASSWORD'));
+if (nconf.get("JIRA_HOST")) nconf.set("jira:host", nconf.get("JIRA_HOST"));
+if (nconf.get("JIRA_USER")) nconf.set("jira:username", nconf.get("JIRA_USER"));
+if (nconf.get("JIRA_PASSWORD"))
+  nconf.set("jira:password", nconf.get("JIRA_PASSWORD"));
 
 // Init Jira client
-const config = nconf.get('jira');
+const config = nconf.get("jira");
 const jira = new jiraClient(config);
 
 // Init GitLab client
-const git = new Gitlab({
-  host: nconf.get('GITLAB_HOST'),
-  token: nconf.get('GITLAB_TOKEN')
-});
+// const git = new Gitlab({
+//   host: nconf.get("GITLAB_HOST"),
+//   token: nconf.get("GITLAB_TOKEN"),
+// });
 
-// Const
-const rapidView = 11040;
-const sprintId = 109066; // Sprint 115
-
-const gitUserId = 17795;
-const gitProjectId = 29679;
-const gitBuildProjectId = 52161;
-
-const lastReportDate = '2021-01-12';
+// REPORTING DATE
+const lastReportDate = "2023-12-01";
 
 // Search for issues
 (async function () {
   try {
-    // const sprints = await jira.getAllSprints(rapidView)
-    // const { id: sprintId } = sprints.values.find(s => s.name === 'Sprint 115')
-
-    console.log('Searching Jira...');
-    const { issues, startAt, maxResults, total } = await jira.searchJira(`assignee = currentUser() and status changed by currentUser() AFTER ${lastReportDate} order by issuetype`);
+    console.log("Searching Jira...");
+    const { issues, startAt, maxResults, total } = await jira.searchJira(
+      // `assignee = currentUser() and status changed by currentUser() AFTER ${lastReportDate} order by issuetype`
+      `assignee = currentUser() and status changed AFTER ${lastReportDate} order by updatedDate ASC`
+    );
+    console.log(`Found ${total} records`);
 
     const stories = [];
+    const tasks = [];
     const bugs = [];
+    const other = [];
 
-    const templateStory = Handlebars.compile("• {{{reportStory issue.key issue.fields}}}");
-    const templateBug = Handlebars.compile("• {{{reportBug issue.key issue.fields}}}");
-  
+    const templateStory = Handlebars.compile(
+      "{{{reportStory issue.key issue.fields}}};"
+    );
+    const templateBug = Handlebars.compile(
+      "{{{reportBug issue.key issue.fields}}};"
+    );
+    const templatePullRequest = Handlebars.compile(
+      "{{{reportPullRequest pullRequest}}};"
+    );
+
+    const issuesKeys = issues.map((issue) => issue.key);
+
     // Iterate through the issues and create an output for each one
     issues.forEach((issue, index) => {
       const context = { issue, index };
 
-      if (issue.fields.issuetype.name === 'Story') {
-        stories.push(templateStory(context));
-      } else if (issue.fields.issuetype.name === 'Bug') {
+      if (issue.fields.issuetype.name === "Story") {
+        stories.push(templateStory(context)); // TODO Delete?
+      } else if (issue.fields.issuetype.name === "Sub-task") {
+        tasks.push(templateStory(context));
+      } else if (issue.fields.issuetype.name === "Bug") {
         bugs.push(templateBug(context));
       } else {
-        console.error(`ERROR: Unknown issue type: ${issue.fields.issuetype.name}`);
+        console.error(
+          `ERROR: Unknown issue type: ${issue.fields.issuetype.name}`
+        );
       }
     });
 
-    console.log('Searching GitLab...');
+    console.log("Searching Bitbucket...");
+    const organizr_url = `${nconf.get(
+      "ORGANIZR_HOST"
+    )}/rest/organizr/latest/pull-requests?q=author%3DcurrentUser()&page=0&size=30`;
+    const pullRequests = await fetch(organizr_url, {
+      method: "get",
+      headers: {
+        Authorization:
+          "Basic " +
+          base64.encode(
+            nconf.get("ORGANIZR_USER") + ":" + nconf.get("ORGANIZR_PASSWORD")
+          ),
+      },
+    }).then((res) => res.json());
+
+    pullRequests.forEach(({ pullRequest }) => {
+      const { updatedDate } = pullRequest;
+
+      if (new Date(updatedDate) >= new Date(lastReportDate)) {
+        // check that at least one key from isses array is contained by the pull request title
+        const found = issuesKeys.some((key) => pullRequest.title.includes(key));
+        if (found) return;
+
+        other.push(templatePullRequest({ pullRequest }));
+      }
+    });
+    console.log(`Found ${other.length} records`);
+
+    console.log(pullRequests[1]);
+
+    /*
+    console.log("Searching GitLab...");
     // const mergeRequests = await git.MergeRequests.all({ authorId: gitUserId, projectId: gitBuildProjectId, createdAfter: lastReportDate, state: 'merged' });
-    const mergeRequests = await git.MergeRequests.all({ authorId: gitUserId, createdAfter: lastReportDate });
+    const mergeRequests = await git.MergeRequests.all({
+      authorId: gitUserId,
+      createdAfter: lastReportDate,
+    });
 
     const other = [];
 
-    const templateMergeRequest = Handlebars.compile("• {{{reportMergeRequest mergeRequest}}}");
-    const templatePromotion = Handlebars.compile("• {{{reportPromotion mergeRequest}}}");
+    const templateMergeRequest = Handlebars.compile(
+      "• {{{reportMergeRequest mergeRequest}}}"
+    );
+    const templatePromotion = Handlebars.compile(
+      "• {{{reportPromotion mergeRequest}}}"
+    );
 
     mergeRequests.forEach((mergeRequest) => {
       if (mergeRequest.project_id === gitProjectId) {
@@ -82,10 +129,12 @@ const lastReportDate = '2021-01-12';
         console.error(`ERROR: Unknown project_id: ${mergeRequest.project_id}`);
       }
     });
+    */
 
     // Print the output
     console.log("\nDevelopment:");
-    console.log(stories.join("\n"));
+    // console.log(stories.join("\n"));
+    console.log(tasks.join("\n"));
     console.log("\nBugfix:");
     console.log(bugs.join("\n"));
     console.log("\nOther:");
